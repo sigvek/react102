eec9fc77bb37b94cd95939df96ba3056a6c58c01
Sigve Kringstad <sigve.kringstad@bouvet.no>
2018-04-12 16:25:43 +0200

    Added root reducer

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react';
+//import PropTypes from 'prop-types';
+
+class CoursesPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: ""}
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave() {
+    alert(`Saving ${this.state.course.title}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <h2>Add course</h2>
+        <input type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+          placeholder="Title" />
+        <input type="submit"
+          value="Save"
+          onClick={this.onClickSave}
+          className="btn btn-primary" />
+      </div>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+
+};
+
+export default CoursesPage;
