0ce62fb68eda418cfae80ac99e9a98079bb13689
Sigve Kringstad <sigve.kringstad@bouvet.no>
2018-04-16 15:38:32 +0200

    More action tests

--- src/actions/courseActions.test.js
+++ src/actions/courseActions.test.js
@@ -18,5 +18,35 @@ describe('Course Actions', () => {
       // assert
       expect(action).toEqual(expectedAction);
     });
+
+    it('should create a UPDATE_COURSE_SUCCESS action.', () => {
+      // arrange
+      const course = {id: 'clean-code', title: 'Clean code'};
+      const expectedAction = {
+        type: types.UPDATE_COURSE_SUCCESS,
+        course: course
+      };
+
+      // act
+      const action = courseActions.updateCourseSuccess(course);
+
+      // assert
+      expect(action).toEqual(expectedAction);
+    });
+
+    it('should create a LOAD_COURSES_SUCCESS action.', () => {
+      // arrange
+      const courses = [{id: 'clean-code', title: 'Clean code'}];
+      const expectedAction = {
+        type: types.LOAD_COURSES_SUCCESS,
+        courses: courses
+      };
+
+      // act
+      const action = courseActions.loadCoursesSuccess(courses);
+
+      // assert
+      expect(action).toEqual(expectedAction);
+    });
   });
 });
