04ff017d4cb7d774f0ff72c5f871779a85a98995
Sigve Kringstad <sigve.kringstad@bouvet.no>
2018-04-18 16:07:04 +0200

    Added Test for course reducer on BEGIN_DELETE_COURSE

--- src/reducers/courseReducer.test.js
+++ src/reducers/courseReducer.test.js
@@ -45,6 +45,28 @@ describe('Course Reducer', () => {
     expect(newState).toExclude(initialState[1]);
   });
 
+  it('should indicate deleting a course when passed BEGIN_DELETE_COURSE action.', () => {
+    // Arrange
+    const initialState = [
+      {id: 'A', title: 'A'},
+      {id: 'B', title: 'B'},
+      {id: 'C', title: 'C'}
+    ];
+    const course = {id: 'B', title: 'New title'};
+    const action = courseActions.deleteCourseBegin(course);
+
+    // Act
+    const newState = courseReducer(initialState, action);
+    const deletingCourse = newState.find(a => a.id == course.id);
+    const untouchedCourse = newState.find(a => a.id == 'A');
+
+    // Assert
+    expect(newState.length).toEqual(3);
+    expect(deletingCourse).toExist();
+    expect(untouchedCourse.isDeleting).toNotExist();
+    expect(deletingCourse.isDeleting).toEqual(true);
+  });
+
   it('should delete a course when passed DELETE_COURSE_SUCCESS action.', () => {
     // Arrange
     const initialState = [
