33311f79be596d418424f87008a2b9915d7b5b90
Sigve Kringstad <sigve.kringstad@bouvet.no>
2018-04-19 14:32:44 +0200

    Fix of bug regarding action type for sorting

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-//import PropTypes from 'prop-types';
 import Header from './common/Header';
 
 class App extends Component {
